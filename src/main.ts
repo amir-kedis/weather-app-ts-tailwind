fe7b80fa6f6a3f02f107c7de99322c0106d5e5d6
amir-kedis <amir.kedis@gmail.com>
2024-02-27 20:27:35 +0200

    🔥 feat: update ui module

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./index.css";
 import "./style.css";
 
 import { getWeather, getQuote } from "./api.ts";
+import updateUI from "./ui.ts";
 
 const weather = await getWeather("London");
 const quote = await getQuote();
 
 console.log(weather);
 console.log(quote);
+
+updateUI({ weather, quote });
---
+++ src/ui.ts
@@ -0,0 +1,42 @@
+import { weatherInfo, quote } from "./api";
+
+type State = {
+  weather: weatherInfo;
+  quote: quote;
+};
+
+const updateUI = (state: State) => {
+  const cityEl = document.getElementById("city") as HTMLParagraphElement;
+  const curWeatherTempEl = document.getElementById(
+    "curWeatherTemp"
+  ) as HTMLParagraphElement;
+  const curWeatherConditionEl = document.getElementById(
+    "curWeatherCondition"
+  ) as HTMLParagraphElement;
+  const curWeatherIconEl = document.getElementById(
+    "curWeatherIcon"
+  ) as HTMLImageElement;
+  const quoteEl = document.getElementById("quote") as HTMLParagraphElement;
+  const authorEl = document.getElementById("author") as HTMLParagraphElement;
+  const forecastEl = document.getElementById("forecast") as HTMLDivElement;
+
+  cityEl.textContent = `${state.weather.city}, ${state.weather.country}`;
+  curWeatherTempEl.textContent = `${state.weather.curWeather.temp}°C`;
+  curWeatherConditionEl.textContent = state.weather.curWeather.condition;
+  curWeatherIconEl.src = state.weather.curWeather.icon;
+  quoteEl.textContent = state.quote.quote;
+  authorEl.textContent = state.quote.author;
+
+  forecastEl.innerHTML = state.weather.forecast
+    .map((day) => {
+      return `
+      <p>${day.date}</p>
+      <p>${day.condition}</p>
+      <p>${day.maxTemp}°C / ${day.minTemp}°C</p>
+      <img src="${day.icon}" alt="${day.condition}" />
+    `;
+    })
+    .join("");
+};
+
+export default updateUI;

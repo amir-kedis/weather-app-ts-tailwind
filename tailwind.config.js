729ed5d8a1cb5c1abf8deb995ca3b6b2c151d291
amir-kedis <amir.kedis@gmail.com>
2024-03-02 21:25:17 +0200

    🧹 chore: import fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "ibm-condensed": "IBM Plex Sans Condensed, sans-serif",
+      "public-sans": "Public Sans, sans-serif",
+    },
   },
   plugins: [],
 };
